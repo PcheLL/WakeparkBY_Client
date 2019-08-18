4cf06820067e7cdaccb2b02d4684a07c8124cb2f
Илья Пчельников <alexmnts1398@gmail.com>
2019-08-18 18:58:00 +0300

    add FloatingActionsMenu

--- app/src/main/java/com/wakeparkby/Fragment/FragmentMaps.java
+++ app/src/main/java/com/wakeparkby/Fragment/FragmentMaps.java
@@ -13,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,7 +29,7 @@ import com.wakeparkby.R;
 /**
  * класс для работы с объектом интерфейса карты
  */
-public class FragmentMaps extends Fragment implements OnMapReadyCallback {
+public class FragmentMaps extends Fragment implements OnMapReadyCallback, View.OnClickListener {
     Button buttonAddCoordinates;
     private MapView mapView;
     private GoogleMap mMap;
@@ -39,9 +41,9 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_maps, container, false);
-    //    SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
-     //   mapFragment.getMapAsync(this);
-     //   latLng = new LatLng(,)
+        //    SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        //   mapFragment.getMapAsync(this);
+        //   latLng = new LatLng(,)
 
         ActionBar toolbar = ((AppCompatActivity) getActivity()).getSupportActionBar();
         toolbar.setTitle("Карта");
@@ -51,9 +53,16 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
         fragmentTransaction.add(R.id.map, mMapFragment);
         fragmentTransaction.commit();
         mMapFragment.getMapAsync(this);
+
+        FloatingActionsMenu rightLabels = (FloatingActionsMenu) rootView.findViewById(R.id.right_labels);
+        FloatingActionButton fab_drozdy = rootView.findViewById(R.id.fab_drozdy);
+        FloatingActionButton fab_logoysk = rootView.findViewById(R.id.fab_logoysk);
+        fab_drozdy.setOnClickListener(this);
+        fab_logoysk.setOnClickListener(this);
         return rootView;
 
     }
+
     public static FragmentMaps newInstance() {
         return new FragmentMaps();
     }
@@ -71,16 +80,16 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
             mMap.animateCamera(zoom);
         }
         else {*/
-            LatLng minsk = new LatLng(53.907211, 27.558678 );
-            CameraUpdate zoom = CameraUpdateFactory.zoomTo(11);
-            CameraUpdate centre = CameraUpdateFactory.newLatLng(minsk);
-            mMap.moveCamera(centre);
-            mMap.animateCamera(zoom);
+        LatLng minsk = new LatLng(53.907211, 27.558678);
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(11);
+        CameraUpdate centre = CameraUpdateFactory.newLatLng(minsk);
+        mMap.moveCamera(centre);
+        mMap.animateCamera(zoom);
 
-        LatLng logoysk = new LatLng(54.181223,27.810689);
+        LatLng logoysk = new LatLng(54.181223, 27.810689);
         googleMap.addMarker(new MarkerOptions().position(logoysk)
                 .title("Логойск"));
-        LatLng drozdy = new LatLng(53.956229,27.448999);
+        LatLng drozdy = new LatLng(53.956229, 27.448999);
         googleMap.addMarker(new MarkerOptions().position(drozdy)
                 .title("Дрозды"));
 
@@ -116,4 +125,34 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
     public void setLatLng(LatLng latLng) {
         this.latLng = latLng;
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab_drozdy:
+                setLatLng(new LatLng(53.956229, 27.448999));
+                //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+                CameraUpdate centre = CameraUpdateFactory.newLatLng(latLng);
+                mMap.moveCamera(centre);
+                mMap.animateCamera(zoom);
+                break;
+            case R.id.fab_logoysk:
+                setLatLng(new LatLng(54.181223, 27.810689));
+                zoom = CameraUpdateFactory.zoomTo(15);
+                centre = CameraUpdateFactory.newLatLng(latLng);
+                mMap.moveCamera(centre);
+                mMap.animateCamera(zoom);
+                break;
+        }
+
+    }
 }
+
+/*
+mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+            CameraUpdate centre = CameraUpdateFactory.newLatLng(latLng);
+            mMap.moveCamera(centre);
+            mMap.animateCamera(zoom);
+ */
